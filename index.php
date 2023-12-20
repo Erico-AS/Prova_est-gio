272fd79be419b5b7e62dcea2801fc320312f6f1e
Érico Augusto <67467324+Erico-AS@users.noreply.github.com>
2023-12-20 11:56:45 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../../components/menu/styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Produtos</title>
+</head>
+<body>
+    <nav>
+        <h1>Bem vindo a sua Biblioteca!</h1>
+    </nav>
+    <main>
+        <a href="./pages/cadastrar">Cadastrar</a>
+        <a href="./pages/listar">Listar</a>
+    </main>
+</body>
+</html>
